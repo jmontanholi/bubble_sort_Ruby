c0fd7a9649d6339a6edb5889b310fd7951b4b7fc
jmontanholi <jonunes9326@hotmail.com.br>
2021-03-17 17:37:18 -0300

    Fix linter spaces problems

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,11 +1,7 @@
 def bubble_sort(arr)
-#a method to iterate through the array items.
-#getting each item to compare them (left and right)
   for i in 0..arr.length-1  do       
     for j in 0..arr.length-2 do     
-      if( arr[j]>arr[j+1]) 
-        arr[j], arr[j+1] = arr[j+1], arr[j]
-      end    
+      arr[j], arr[j + 1] = arr[j + 1], arr[j] if(arr[j] > arr[j + 1])
      end
   end
   return arr
@@ -14,11 +10,10 @@ end
 def bubble_sort_by(arr)
   for i in 0..arr.length-1  do
     for j in 0..arr.length-2 do
-      result = yield(arr[j], arr[j+1])
-      if result > 0
-        arr[j], arr[j+1] = arr[j+1], arr[j]
-      end
+      result = yield(arr[j],arr[j + 1])
+      arr[j], arr[j + 1] = arr[j + 1], arr[j] if result > 0
     end
   end
   return arr
 end
+

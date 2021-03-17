55e195495bb5a2b3b2ea4a9741fe50bf02870f93
jmontanholi <jonunes9326@hotmail.com.br>
2021-03-17 20:28:08 -0300

    Fix errors found by code reviewer and linter

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,6 +1,6 @@
 def bubble_sort(arr)
   arr.each_index do |i|
-    arr[i], arr[i + 1] = arr[i + 1], arr[i] if !i == (arr.length - 1) && (arr[i] > arr[i + 1])
+    arr[i], arr[i + 1] = arr[i + 1], arr[i] if i != arr.length - 1 && (arr[i] > arr[i + 1])
   end
   arr
 end
@@ -14,3 +14,9 @@ def bubble_sort_by(arr)
   end
   arr
 end
+
+test1 = [3, 1, 2, 5, 6, 8, 7, 10, 9, 4]
+
+p bubble_sort(test1)
+
+p bubble_sort_by(%w[hiya hey hi h hello]) { |left, right| left.length - right.length }

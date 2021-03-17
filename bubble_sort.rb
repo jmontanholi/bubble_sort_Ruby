671c89132c8544b2ba490341fa0634036c729707
jmontanholi <jonunes9326@hotmail.com.br>
2021-03-17 18:53:16 -0300

    Fix code reviewer request and linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,17 +1,15 @@
 def bubble_sort(arr)
   arr.each_index do |i|
-    arr.each_index do |j|
-      arr[i], arr[j] = arr[j], arr[i] if arr[i] < arr[j]
-    end
+    arr[i], arr[i + 1] = arr[i + 1], arr[i] if !i == (arr.length - 1) && (arr[i] > arr[i + 1])
   end
   arr
 end
 
 def bubble_sort_by(arr)
   arr.each_index do |i|
-    arr.each_index do |j|
-      result = yield(arr[i], arr[j])
-      arr[i], arr[j] = arr[j], arr[i] if result.negative?
+    unless i == arr.length - 1
+      result = yield(arr[i], arr[i + 1])
+      arr[i], arr[i + 1] = arr[i + 1], arr[i] if result.positive?
     end
   end
   arr

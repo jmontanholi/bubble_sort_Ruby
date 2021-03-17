c37f0f666edbdf2b9ed76e978107f4ae915b18e0
jmontanholi <jonunes9326@hotmail.com.br>
2021-03-17 17:46:22 -0300

    Fix extra spaces linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,24 +1,18 @@
 def bubble_sort(arr)
-  arr.each_index do |i|    
-    arr.each_index do |j|   
+  arr.each_index do |i|
+    arr.each_index do |j|
       arr[i], arr[j] = arr[j], arr[i] if(arr[i] < arr[j])
-     end
+    end
   end
-  return arr
+  arr
 end
 
 def bubble_sort_by(arr)
   arr.each_index do |i|
     arr.each_index do |j|
-      result = yield(arr[i],arr[j])
+      result = yield(arr[i], arr[j])
       arr[i], arr[j] = arr[j], arr[i] if result < 0
     end
   end
-  return arr
+  arr
 end
-
-test_arr = [1,2,3,4,7,9,1,3,2,6]
-second_arr = ["hi", "hello", "hey"]
-
-p bubble_sort(test_arr)
-p bubble_sort_by(second_arr){|left,right|left.length - right.length}

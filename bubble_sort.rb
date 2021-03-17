bcbdd9d0e530703477ce5f3ff06c0e74a77bf26c
Rolando Alvarez <rolandoalvarezfaye@gmail.com>
2021-03-17 15:51:49 -0400

    finish bubble sort 1

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,10 +1,17 @@
 def bubble_sort(arr)
 #a method to iterate through the array items.
 #getting each item to compare them (left and right)
-  
+  for i in 0..arr.length-1  do       
+    puts arr[i]  
+    for j in 0..arr.length-2 do     
+      if( arr[j]>arr[j+1]) 
+        arr[j], arr[j+1] = arr[j+1], arr[j]
+      end    
+     end
+  end
   return arr
 end
 
 test_arr = [1,2,3,4,7,9,1,3,2,6]
 
-puts bubble_sort(test_arr)
+p bubble_sort(test_arr)

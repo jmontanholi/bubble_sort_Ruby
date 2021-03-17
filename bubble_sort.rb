7097ac97710c82ca623a0693f00076cbf04b6ee7
jmontanholi <jonunes9326@hotmail.com.br>
2021-03-17 16:37:26 -0300

    Start bubble_sort file, define method

---
+++ bubble_sort.rb
@@ -0,0 +1,10 @@
+def bubble_sort(arr)
+#a method to iterate through the array items.
+#getting each item to compare them (left and right)
+  
+  return arr
+end
+
+test_arr = [1,2,3,4,7,9,1,3,2,6]
+
+puts bubble_sort(test_arr)

85989072e798bd5334b204603ae63338be5c1f0e
jmontanholi <jonunes9326@hotmail.com.br>
2021-03-17 17:19:34 -0300

    Finish bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -12,23 +12,13 @@ def bubble_sort(arr)
 end
 
 def bubble_sort_by(arr)
-  #a method to iterate through the array items.
-  #getting each item to compare them (left and right)
-    for i in 0..arr.length-1  do        
-      for j in 0..arr.length-2 do     
-        if( arr[j].length>arr[j+1].length) 
-          arr[j], arr[j+1] = arr[j+1], arr[j]
-        end    
-       end
+  for i in 0..arr.length-1  do
+    for j in 0..arr.length-2 do
+      result = yield(arr[j], arr[j+1])
+      if result > 0
+        arr[j], arr[j+1] = arr[j+1], arr[j]
+      end
     end
-    return arr
   end
-  
-
-test_arr = [1,2,3,4,7,9,1,3,2,6]
-second_arr = ["hi", "hello", "hey"]
-
-p bubble_sort(test_arr)
-p bubble_sort_by(second_arr) do |left,right|
-    left.length - right.length
-  end
+  return arr
+end

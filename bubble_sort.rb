16e9b9b79e3c3ea415f0d6cd0af0f0e3f9e263c7
jmontanholi <jonunes9326@hotmail.com.br>
2021-03-18 11:21:00 -0300

    Finish code reviewer request and linter

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,32 +1,24 @@
 def bubble_sort(arr)
-  arr.each_index do |i|
-    arr[i], arr[i + 1] = arr[i + 1], arr[i] if i != arr.length - 1 && (arr[i] > arr[i + 1])
-  end
-  arr
-end
+  arr.each_index do |j|
+    next if j == arr.length
 
-def bubble_sort_by(arr)
-  arr.each_index do |i|
-    unless i == arr.length - 1
-      result = yield(arr[i], arr[i + 1])
-      arr[i], arr[i + 1] = arr[i + 1], arr[i] if result.positive?
+    arr.each_index do |i|
+      arr[i], arr[i + 1] = arr[i + 1], arr[i] if i != arr.length - 1 && (arr[i] > arr[i + 1])
     end
   end
   arr
 end
 
-test1 = [3, 1, 2, 5, 6, 8, 7, 10, 9, 4]
+def bubble_sort_by(arr)
+  arr.each_index do |j|
+    next if j == arr.length
 
-#   ATTENTION PLEASE
-#   THIS IS NOT DEBUG
-#   THESE ARE EXAMPLES FOR CODE REVIEWERS
-#   Bubble_sort and bubble_sort_by are working perfectly
-#   according to this video from harvard explaining the problem:
-#   https://www.youtube.com/watch?v=8Kp-8OGwphY
-
-p bubble_sort(test1)
-p bubble_sort(bubble_sort(test1))
-p bubble_sort(bubble_sort(bubble_sort(test1)))
-p bubble_sort_by(%w[hiya hey hi h hello]) { |left, right| left.length - right.length }
-p bubble_sort_by(%w[hey hi h hiya hello]) { |left, right| left.length - right.length }
-p bubble_sort_by(%w[hi h hey hiya hello]) { |left, right| left.length - right.length }
+    arr.each_index do |i|
+      unless i == arr.length - 1
+        result = yield(arr[i], arr[i + 1])
+        arr[i], arr[i + 1] = arr[i + 1], arr[i] if result.positive?
+      end
+    end
+  end
+  arr
+end

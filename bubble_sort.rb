9865af94b119bceb3920fe12367feac9fb5f0602
Rolando Alvarez <rolandoalvarezfaye@gmail.com>
2021-03-18 10:48:50 -0400

    add sorted value

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,10 +1,17 @@
 def bubble_sort(arr)
   arr.each_index do |j|
-    next if j == arr.length
+    next if j == arr.length 
 
+    sorted = true
     arr.each_index do |i|
-      arr[i], arr[i + 1] = arr[i + 1], arr[i] if i != arr.length - 1 && (arr[i] > arr[i + 1])
+      if i != arr.length - 1 && (arr[i] > arr[i + 1]) then
+        arr[i], arr[i + 1] = arr[i + 1], arr[i] 
+        sorted = false
+      end
+      
     end
+
+    break if sorted
   end
   arr
 end
@@ -13,12 +20,21 @@ def bubble_sort_by(arr)
   arr.each_index do |j|
     next if j == arr.length
 
+    sorted = true
     arr.each_index do |i|
       unless i == arr.length - 1
         result = yield(arr[i], arr[i + 1])
-        arr[i], arr[i + 1] = arr[i + 1], arr[i] if result.positive?
+        if result.positive? then
+          arr[i], arr[i + 1] = arr[i + 1], arr[i] 
+          sorted = false;
+        end
       end
     end
+
+    break if sorted
   end
   arr
 end
+
+p bubble_sort([3, 1, 2, 5, 6, 8, 7, 10, 9, 4, 11])
+p bubble_sort_by(["hiya", "hey", "hi", "h", "hello"]) { |left, right| left.length - right.length }

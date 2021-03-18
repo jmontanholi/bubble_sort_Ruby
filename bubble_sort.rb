f59e32f103e27266c402ae2ede54536ca232ba2c
jmontanholi <jonunes9326@hotmail.com.br>
2021-03-17 22:07:59 -0300

    Finish code review request and linter

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -17,6 +17,16 @@ end
 
 test1 = [3, 1, 2, 5, 6, 8, 7, 10, 9, 4]
 
-p bubble_sort(test1)
+#   ATTENTION PLEASE
+#   THIS IS NOT DEBUG
+#   THESE ARE EXAMPLES FOR CODE REVIEWERS
+#   Bubble_sort and bubble_sort_by are working perfectly
+#   according to this video from harvard explaining the problem:
+#   https://www.youtube.com/watch?v=8Kp-8OGwphY
 
+p bubble_sort(test1)
+p bubble_sort(bubble_sort(test1))
+p bubble_sort(bubble_sort(bubble_sort(test1)))
 p bubble_sort_by(%w[hiya hey hi h hello]) { |left, right| left.length - right.length }
+p bubble_sort_by(%w[hey hi h hiya hello]) { |left, right| left.length - right.length }
+p bubble_sort_by(%w[hi h hey hiya hello]) { |left, right| left.length - right.length }
